3ad425de1aac30112bd2082f3f3be5b2e0180919
Victor Glicério <66135800+ImGlic@users.noreply.github.com>
2024-08-04 17:37:31 -0300

    ajustes treemap

--- index.php
+++ index.php
@@ -6,19 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>COVID-19 Tree Map - Brasil</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <link href="./css/index.css" rel="stylesheet">
 
 </head>
 
 <body>
     <div class="container">
-        <div class="row ">
+        <div class="row">
             <div class="col-md-8 text-center">
                 <h1>COVID-19 Tree Map - Brasil</h1>
                 <select id="mesAno" class="form-control mb-3">
-                    <option value="" disabled selected>Selecione o mês</option>
+                    <option value="" disabled selected>Selecione o mês e ano</option>
                 </select>
-                <div id="relatorio"></div>
+                <div id="relatorio" class="mt-4"></div>
             </div>
         </div>
     </div>
@@ -33,15 +34,15 @@
                     </button>
                 </div>
                 <div class="modal-body">
-                    <div class="grafico-pizza mb-5">
+                    <div class="grafico-pizza">
                         <h4>Casos (<b id="qtdCasos"></b>)</h4>
                         <canvas id="casosGrafico"></canvas>
                     </div>
-                    <div class="grafico-pizza mb-5">
+                    <div class="grafico-pizza">
                         <h4>Vacinação (<b id="qtdVacinacao"></b>)</h4>
                         <canvas id="vacinacaoGrafico"></canvas>
                     </div>
-                    <div class="grafico-pizza mb-5">
+                    <div class="grafico-pizza">
                         <h4>Mortes (<b id="qtdMortes"></b>)</h4>
                         <canvas id="mortesGrafico"></canvas>
                     </div>
@@ -53,14 +54,11 @@
         </div>
     </div>
 
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="js/index.js"></script>
 </body>
-
-<script src="js/index.js"></script>
-<!-- Adiciona o JavaScript do Bootstrap e as dependências -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script src="./js/index.js"></script>
 
 </html>
